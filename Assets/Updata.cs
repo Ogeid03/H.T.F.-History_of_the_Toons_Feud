25f29d19cc49e5c43ea7d023b0b8d99fbc7041e7
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2024-10-10 12:41:40 +0200

    Micael_Fawn Update
    Walk Animation
    Effects Downloaded
    Scripts corrections

--- Assets/Updata.cs
+++ Assets/Updata.cs
@@ -4,7 +4,7 @@ Update da fare:
     - implementare l'incremento dello spawn in base al "sottolivello" della stanza
     - Sviluppare altre tipologie di nemici (Tank) e il boss con caratteristiche ambientali per la boss battle
     - migliorare il combattimento
-    - impostare le animazioni e le nuove skin di Martina
+    - impostare le animazioni e le nuove skin di Martina (Done: Tomato, Micael Faws - To Do: Enemies, Boss, Ambienti?, Ink)
     - sistemare le varie stanze
     - grafica per GUI
     - gestione dei due livelli
@@ -12,4 +12,16 @@ Update da fare:
     - gestione del medikit
     - aggiungere dei veri ascensori
     - aggiungere un ambiente esterno
+    - aggiunta di suoni ed effetti (Gli effetti sono dentro Imports)
+    
+Fixes:
+    - animazioni
+    - morte
+    - intro
+    - hitted by tomatoes
+
+Extras:
+    - Schermata iniziale
+    - Salvataggi
+    - Multiplayer
 */
