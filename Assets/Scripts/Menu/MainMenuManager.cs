1eeee1375c204600d213ea2c4ea392cdfcb27ccd
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2024-11-14 11:33:11 +0100

    MainMenu

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Metodo per iniziare il gioco
+    public void PlayGame()
+    {
+        Debug.Log("PlayGame button pressed");
+        // Carica la scena di gioco, sostituisci "GameScene" con il nome della tua scena di gioco
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    // Metodo per uscire dal gioco 
+    /*public void QuitGame()
+    {
+        // Chiude l'applicazione
+        Application.Quit();
+        Debug.Log("Game Closed"); // Solo per debug, non si vedrà nell'app finale
+    }*/
+}
