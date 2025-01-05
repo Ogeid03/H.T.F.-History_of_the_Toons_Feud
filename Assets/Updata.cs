473bcbec3358369331e9157f1931e5b84906f9dd
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2025-01-05 18:37:25 +0100

    analisi mancanze
    aggiunta png ascensore
    aggiunta roccia
    problemi ascensore in introScene

--- Assets/Updata.cs
+++ Assets/Updata.cs
@@ -1,7 +1,7 @@
 ﻿/*
 Update da fare:
     ✅ - sviluppare una stanza finale per ogni livello
-    migliorare il combattimento e caratteristiche nemici
+    migliorare il combattimento e ✅ caratteristiche nemici
     impostare le animazioni e le nuove skin di Martina (Done: Tomato, Micael Faws, Enemies, PitcherEnemies - To Do: Boss, Ink)
     ✅ - sistemare le varie stanze
     ✅ - grafica per GUI (pulsanti)  
@@ -9,18 +9,27 @@ Update da fare:
     ✅ - aggiungere dei veri ascensori   
     ✅ - aggiungere un ambiente esterno (portale Warner SIS)
     aggiunta di suoni ed effetti (Gli effetti sono dentro Imports)
-    introduzione della "leva" per aprire le porte di qualche livello
-    colorare GUI
+    ✅colorare GUI
 
 Fixes:   
     movement
     animazioni
     ✅ - morte
+    intro (se premuti i tasti di movimento, aggiunta testi "clicca per continuare", riquadri per evidenziare i tasti)
+    porte con nasto pellicola
+    
 intro
-    hitted by tomatoes
-    ✅ - spawn medikit✅ - adattamento GUI✅ - fix animazione vibrante dell'ascensore e 2x✅ - cambiare il player con la texture HD di Martina
-    far ittare l'inchiostro sulla testa
-    ✅ - fix i tasti per evitare di saltare e sparare✅ - sistemare la GUI troppo piccola✅ - bloccare la GUI✅ - fixare vita boss✅ - danieleSgerro non può colpire oltre il ground✅ - danieleSgerroRespawn deve fare danno
+    hitted by tomatoes, far ittare l'inchiostro sulla testa
+    ✅ - spawn medikit
+    ✅ - adattamento GUI
+    ✅ - fix animazione vibrante dell'ascensore e 2x
+    ✅ - cambiare il player con la texture HD di Martina
+    ✅ - fix i tasti per evitare di saltare e sparare
+    ✅ - sistemare la GUI troppo piccola
+    ✅ - bloccare la GUI
+    ✅ - fixare vita boss
+    ✅ - danieleSgerro non può colpire oltre il ground
+    ✅ - danieleSgerroRespawn deve fare danno
     visuale stanze alte
 
 Extras:
@@ -29,4 +38,7 @@ Extras:
     Multiplayer
     gestione dei due livelli
     gestione dei caricamenti
+
+DISMISSED:
+    introduzione della "leva" per aprire le porte di qualche livello
 */
